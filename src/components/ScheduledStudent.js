5c89e25a6b39536eebf93e92203744a4498af5e1
Sai Vijay Nagarur <78963023+VIJAYRUR@users.noreply.github.com>
2023-09-22 14:16:51 +0530

    Update ScheduledStudent.js

--- src/components/ScheduledStudent.js
+++ src/components/ScheduledStudent.js
@@ -1,37 +1,50 @@
 import React, { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUniversity, faGraduationCap, faCode } from "@fortawesome/free-solid-svg-icons"; // Import the icons you need
+import {
+  faUniversity,
+  faGraduationCap,
+  faCode,
+  faClock,
+  faEnvelope,
+  faCalendar
+} from "@fortawesome/free-solid-svg-icons"; // Import the icons you need
+
+const cardHoverStyles = {
+  transition: "transform 0.2s, box-shadow 0.2s",
+  boxShadow: "0 0 5px rgba(0, 0, 0, 0.2)",
+};
 
 const ScheduledStudent = () => {
   const [scheduledRequest, setScheduledRequest] = useState(null);
   const [error, setError] = useState("");
+  const [loading, setLoading] = useState(true);
+  const [hoveredCardId, setHoveredCardId] = useState(null);
 
   useEffect(() => {
     const fetchScheduledRequest = async () => {
       try {
-        const response = await fetch("https://proconnect-backend.onrender.com/user/view_student_schedule", {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-            authorization: "Bearer " + localStorage.getItem("token"),
-          },
-        });
+        const response = await fetch(
+          "https://proconnect-backend.onrender.com/user/view_student_schedule",
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+              authorization: "Bearer " + localStorage.getItem("token"),
+            },
+          }
+        );
 
         if (!response.ok) {
           const errorMessage = await response.text();
           setError(errorMessage);
         } else {
           const data = await response.json();
-          if (data[0] == null) {
-            console.log("hi");
-            setScheduledRequest("");
-          } else {
-            setScheduledRequest(data);
-            console.log(data);
-          }
+          setScheduledRequest(data);
         }
       } catch (error) {
         setError("Network error. Please try again later.");
+      } finally {
+        setLoading(false);
       }
     };
 
@@ -40,52 +53,63 @@ const ScheduledStudent = () => {
 
   return (
     <div className="container mt-5">
-      <h2>Scheduled Request</h2>
-      {(error || scheduledRequest === "") && (
+      <h2 className="mb-4">
+        Scheduled Requests
+      </h2>
+      {loading ? (
+        <p>Loading...</p>
+      ) : error || !scheduledRequest ? (
         <div className="alert alert-danger">No scheduled Requests</div>
-      )}
-      {scheduledRequest ? (
-        scheduledRequest.length > 0 ? (
-          <div className="card mt-3">
-            <div className="card-body">
-              {scheduledRequest[0].map((request, index) => (
-                <div key={index}>
-                  <div>
-                    <strong>Date:</strong> {request.date}
-                  </div>
-                  <div>
-                    <strong>Time:</strong> {request.time}
-                  </div>
-                  <div>
-                    <strong>Interviewer Name:</strong>{" "}
-                    {request.interviewer_name}
-                  </div>
-                  <div>
-                    <strong>Interviewer Email:</strong>{" "}
-                    {request.interviewer_email}
-                  </div>
-                  <div>
-                    <strong>Interviewer Company:</strong>{" "}
-                    {request.interviewer_company}
-                  </div>
-                  <div>
-                    <strong>Interviewer Role:</strong>{" "}
-                    {request.interviewer_role}
-                  </div>
-                  <div>
+      ) : scheduledRequest.length === 0 ? (
+        <p>No scheduled requests found.</p>
+      ) : (
+        <div className="row">
+          {scheduledRequest[0].map((request, index) => (
+            <div
+              key={index}
+              className="col-md-4 mb-4"
+              onMouseEnter={() => setHoveredCardId(index)}
+              onMouseLeave={() => setHoveredCardId(null)}
+            >
+              <div
+                className={`card ${
+                  hoveredCardId === index ? "hovered-card" : ""
+                }`}
+                style={hoveredCardId === index ? cardHoverStyles : {}}
+              >
+                <div className="card-body">
+                  <h5 className="card-title">Scheduled Request <i>{index+1}</i></h5>
+                  <p>
+                    <marquee>
+                      <strong>Date: </strong>
+                      {request.date}, <strong>Time:</strong> {request.time}
+                    </marquee>
+                  </p>
+                  <p>
+                    <FontAwesomeIcon icon={faEnvelope} className="mr-2" />{" "}
+                    <strong>Interviewer Email:</strong> {request.interviewer_email}
+                  </p>
+                  <p>
+                    <FontAwesomeIcon icon={faUniversity} className="mr-2" />{" "}
+                    <strong>Interviewer Company:</strong> {request.interviewer_company}
+                  </p>
+                  <p>
+                    <FontAwesomeIcon icon={faGraduationCap} className="mr-2" />{" "}
+                    <strong>Interviewer Role:</strong> {request.interviewer_role}
+                  </p>
+                  <p>
+                    <FontAwesomeIcon icon={faCode} className="mr-2" />{" "}
                     <strong>Interviewer Experience:</strong>{" "}
                     {request.interviewer_experience}
-                  </div>
-                  <i>Make further communication with Interviewer by email</i>
+                  </p>
+                  <p>
+                    <i>Make further communication with Interviewer by email</i>
+                  </p>
                 </div>
-              ))}
+              </div>
             </div>
-          </div>
-        ) : (
-          <p>No scheduled requests found.</p>
-        )
-      ) : (
-        <p></p>
+          ))}
+        </div>
       )}
     </div>
   );
