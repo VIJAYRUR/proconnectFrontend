2222704a2cdfeaa0abb5008013de381def390e04
Sai Vijay Nagarur <78963023+VIJAYRUR@users.noreply.github.com>
2023-09-22 14:20:35 +0530

    Update ProfessionalHistory.js

--- src/components/ProfessionalHistory.js
+++ src/components/ProfessionalHistory.js
@@ -1,19 +1,39 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faUniversity,
+  faGraduationCap,
+  faCode,
+  faMessage,
+  faClock,
+  faEnvelope,
+  faCalendar
+} from "@fortawesome/free-solid-svg-icons"; // Import the icons you need
+
+const cardHoverStyles = {
+  transition: "transform 0.2s, box-shadow 0.2s",
+  boxShadow: "0 0 5px rgba(0, 0, 0, 0.2)",
+};
 
 const ProfessionalHistory = () => {
   const [feedbackHistory, setFeedbackHistory] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [hoveredCardId, setHoveredCardId] = useState(null);
 
   useEffect(() => {
     // Fetch feedback history for the professional here
     const fetchFeedbackHistory = async () => {
       try {
-        const response = await fetch("https://proconnect-backend.onrender.com/user/professional_history", {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-            authorization: "Bearer " + localStorage.getItem("token"),
-          },
-        });
+        const response = await fetch(
+          "https://proconnect-backend.onrender.com/user/professional_history",
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+              authorization: "Bearer " + localStorage.getItem("token"),
+            },
+          }
+        );
 
         if (response.ok) {
           const data = await response.json();
@@ -26,6 +46,8 @@ const ProfessionalHistory = () => {
         }
       } catch (error) {
         console.error("Network error:", error);
+      } finally {
+        setLoading(false);
       }
     };
 
@@ -33,23 +55,54 @@ const ProfessionalHistory = () => {
   }, []);
 
   return (
-    <div className="container mt-5">
-      <h2 className="mb-4">Feedback History</h2>
-      {feedbackHistory.length === 0 ? (
-        <p>No feedback history found.</p>
+    <div className="container mt-2">
+      <h2 className="mb-4" style={{ marginTop: "10%" }}>
+        Feedback History
+      </h2>
+      {loading ? (
+        <p>Loading...</p>
+      ) : feedbackHistory.length === 0 ? (
+        <div className="alert alert-danger">No feedback history found.</div>
       ) : (
-        <ul className="list-group">
+        <div className="row">
           {feedbackHistory.map((feedback, index) => (
-            <li key={index} className="list-group-item">
-              <p>
-                Interview with <strong>{feedback.student_name}</strong>
-              </p>
-              <p>Message: {feedback.message}</p>
-              <p>Date: {feedback.date}</p>
-              <p>Time: {feedback.time}</p>
-            </li>
+            <div
+              key={index}
+              className="col-md-4 mb-4"
+              onMouseEnter={() => setHoveredCardId(index)}
+              onMouseLeave={() => setHoveredCardId(null)}
+            >
+              <div
+                className={`card ${
+                  hoveredCardId === index ? "hovered-card" : ""
+                }`}
+                style={hoveredCardId === index ? cardHoverStyles : {}}
+              >
+                <div className="card-body">
+                  
+                  <p>
+                    <FontAwesomeIcon icon={faCalendar} className="mr-2" />{" "}
+                    <strong>Date:</strong> {feedback.date}
+                  </p>
+                  <p>
+                    <FontAwesomeIcon icon={faClock} className="mr-2" />{" "}
+                    <strong>Time:</strong> {feedback.time}
+                  </p>
+                  <p>
+                    <FontAwesomeIcon icon={faEnvelope} className="mr-2" />{" "}
+                    <strong>Student Email:</strong> {feedback.student_email}
+                  </p>
+                  
+                  <p>
+                    <FontAwesomeIcon icon={faMessage} className="mr-2" />{" "}
+                    <strong>Messsage:</strong> {feedback.message}
+                  </p>
+
+                </div>
+              </div>
+            </div>
           ))}
-        </ul>
+        </div>
       )}
     </div>
   );
